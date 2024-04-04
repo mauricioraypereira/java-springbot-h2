f7364accce473644e97b1a25bdb0de269c4ba4a4
Maurício Ray <mauricioraypereira@gmail.com>
2024-04-03 21:18:22 -0300

    Mew Entity Payment, one-to-one association

--- src/main/java/ray/mauricio/spring/config/TestConfig.java
+++ src/main/java/ray/mauricio/spring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import ray.mauricio.spring.entities.Category;
 import ray.mauricio.spring.entities.Order;
 import ray.mauricio.spring.entities.OrderItem;
+import ray.mauricio.spring.entities.Payment;
 import ray.mauricio.spring.entities.Product;
 import ray.mauricio.spring.entities.User;
 import ray.mauricio.spring.entities.enums.OrderStatus;
@@ -85,5 +86,9 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2024-02-01T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);		
 	}
 }
